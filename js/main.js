3436f4e066590743c9b4304c80cbd790265bce53
aleksadr_lavrov <lavrovweb@gmail.com>
2024-09-19 23:39:45 +0300

    sort by checked

--- js/main.js
+++ js/main.js
@@ -1,30 +1,79 @@
 window.addEventListener('DOMContentLoaded', function(){
     const form = this.document.querySelector('.form');
     const input = this.document.querySelector('.input');
+    const container = this.document.querySelector('.container');
 
-    const allData = [];
+    let allData = [];
+
+    render(allData);
 
     form.addEventListener('submit', function(e){
         e.preventDefault();
-        
-        let id = 0;
 
+        //id calculation for every task
+        let id = 0;
         if(allData.length > 0){
             let currentTask = allData[allData.length - 1];
-            console.log(currentTask);
             let currentId = currentTask.id;
-            console.log(currentId);
             id = currentId += 1;
         }
-
+        //getting value from input and adding data to allData
         const task = {
             name: input.value,
             isChecked: false,
+            status: 'active',
             id: id,
         }
-
         allData.push(task);
         input.value = '';
-
+        render(allData);
     })
+    //render task
+    function render(allData){
+        container.innerHTML = ' ';
+
+        allData.forEach((elem, index) => {
+            container.innerHTML += `
+            <div class="task" data-id="${elem.id}">
+                <span class="task-id">${elem.id}</span>
+                <div class="task-name">${elem.name}</div>
+                <input type="checkbox" ${elem.isChecked ? 'checked' : ''} class="task-check">
+                <button data-id="${elem.id}" class="task-delete">Delete</button>
+            </div>
+            `
+        });
+    }
+    
+    container.addEventListener('click', function(e){
+        //deleting task
+        if(e.target.classList.contains('task-delete')){
+            const id = Number(e.target.dataset.id);
+
+            const targetId = allData.findIndex(function(item){
+                if(item.id === id){
+                    return true;
+                }
+            })
+
+            allData.splice(targetId, 1)
+            render(allData);
+        }
+
+        if(e.target.classList.contains('task-check')){
+            const id = Number(e.target.parentNode.dataset.id);
+            const targetId = allData.findIndex(item => {
+                if(item.id === id){
+                    return true;
+                }
+            })
+            allData[id].isChecked = e.target.checked;
+
+            const activeTask = [];
+            const complitedTask = [];
+
+            render(allData.sort((a,b) => {return a.isChecked - b.isChecked}));
+        }
+    })
+
+    
 })
